17f40b17f461fb4b16e56008332c0b60541f6610
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-01-26 14:03:31 +0900

    mainで開始処理とかの追加をやったよ

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+//グローバルな名前を持つ蒲田エンジンを作成
+using namespace KamataEngine;
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	//============================
+	//初期化
+	//============================
+
+
+	//蒲田エンジンのロード
+	KamataEngine::Initialize(L"AE2?_99_ヤマト_ユウヤ_AL2");
+
+	//DirectXのロード
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	//ゲームシーンの呼び出し
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+
+
+	//ここから下のクラスのインスタンスなどの情報を追加
+
+
+
+	//============================
+	//更新処理
+	//============================
+
+	while (true) {
+
+		//蒲田エンジンのアップデート
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// この中に更新処理などを随時追加していく
+
+		gameScene->Updatta();
+
+		// DirectXの描画処理を追加
+		dxCommon->PreDraw(); // ここから開始して
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw(); // ここで終了
+
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+	//ゲームの終了
+	KamataEngine::Finalize();
+	return 0;
+}
