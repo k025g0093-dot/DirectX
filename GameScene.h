7f7b43ce4fc8f7d688f80adaa14c74ec8344df37
yamato_yuuya <k025g0093@g.neec.ac.jp>
2026-01-26 15:01:57 +0900

    Add files via upload

---
+++ GameScene.h
@@ -0,0 +1,18 @@
+#pragma once
+class GameScene {
+
+public:
+	// 初期化関数
+	void Initialize();
+
+	// 更新処理関数
+	void Updatta();
+
+	// 描画処理
+	void Draw();
+
+private:
+	//プライベートでシーンごとの旋回用の処理関数を追加
+
+
+};
