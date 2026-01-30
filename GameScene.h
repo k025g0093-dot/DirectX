7792d18594ff8370e45eaa67cc5124cfec45b5b9
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-01-31 00:29:44 +0900

    Revert "AL2_05_01をやってみたよ"
    This reverts commit cdf88bc667c8e60e4ad517dbbbe53463c731e618.

--- GameScene.h
+++ GameScene.h
@@ -1,21 +1,26 @@
-﻿#pragma once
+#pragma once
 #include "KamataEngine.h"
-#include"Player.h"
 
 class GameScene {
 
 public:
+	
+	//コンストラクタとデストラクタ
 	GameScene();
 	~GameScene();
 
-	// ワールドトランスフォーム
-	KamataEngine::WorldTransform worldTransform_;
-	// カメラ
-	KamataEngine::Camera camera_;
-
-	//3Dモデルで必要なモデルの呼び出し
+	KamataEngine::Sprite* sprite_ = nullptr;
 	KamataEngine::Model* model_ = nullptr;
 
+	//ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+	//カメラ
+	KamataEngine::Camera camera_;
+
+	//デバック用カメラ
+	KamataEngine::DebugCamera* debugCamera_ = nullptr;
+
+	
 
 	// 初期化関数
 	void Initialize();
@@ -26,10 +31,19 @@ public:
 	// 描画処理
 	void Draw();
 
-	Player* player_ = nullptr;
-
 private:
-	uint32_t textureHandle_;
 
-	// プライベートでシーンごとの旋回用の処理関数を追加
+	float inputFloat[3] = {0, 0, 0};
+
+	//画像の読み込み
+	uint32_t textureHandle_ = 0;
+
+	//サウンドデータ読み込み
+	uint32_t soundDataHandle_ = 0;
+	//音楽再生ハンドル
+	uint32_t voiceHandle_ = 0;
+
+	//プライベートでシーンごとの旋回用の処理関数を追加
+
+
 };
