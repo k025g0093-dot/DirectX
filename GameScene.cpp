13084ecddc8b745fc88b59906d62bd033dd0e61f
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-01-31 00:24:02 +0900

    AL2_05_01をやってみたよ

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,111 +1,70 @@
-#include "GameScene.h"
+﻿#include "GameScene.h"
 //===================================================
 // パブリックの処理
 //===================================================
 
 using namespace KamataEngine;
 
- //コンストラクタ
- GameScene::GameScene() {
+// コンストラクタ
+GameScene::GameScene() { 
 	Initialize();
- }
+
+}
 
 void GameScene::Initialize() {
 
-	// 初期化処理の追加
-
-	// 関数を使っての初期化などあとはnew関連も
+	//初期化処理の追加
 	//  トランスフォームとカメラの初期化
 	worldTransform_.Initialize();
 	camera_.Initialize();
-
 	PrimitiveDrawer::GetInstance()->SetCamera(&camera_);
 
-	debugCamera_ = new DebugCamera(1280, 720);
+	// モデルの生成
+	model_ = Model::Create();
 
-	//ブレンダーみたいな表示線の関数初期化
-	AxisIndicator::GetInstance()->SetVisible(true);
+	// 関数を使っての初期化などあとはnew関連も
 
-	AxisIndicator::GetInstance()->SetTargetCamera(&debugCamera_->GetCamera());
 
-	// 音楽や画像などを読み込む場所さらにはそれらを使ものも入れる
 
 #pragma region 画像読み込み範囲
 
-	textureHandle_ = TextureManager::Load("./Resources/mario.jpg");
-#pragma endregion
+	textureHandle_ = TextureManager::Load("./Resources/uvChecker.png");
+#pragma endregion 
 
-	// 音楽読み込みここから
-#pragma region 音楽読み込み範囲
-	soundDataHandle_ = Audio::GetInstance()->LoadWave("./Resources/fanfare.wav"); // 音楽読み込み
+	
 
-	// ここまで
-#pragma endregion
 
-	// その他の初期化処理
-	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_, true); // 音楽がループで再生するかの
+	//引数などの受け渡しの関係上ここから下にplayerとかの初期化関数とかを追加
 
-	// スプライトとモデルの作成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	model_ = Model::Create();
+	// playerや敵などのインスタンスの生成
+	player_ = new Player();
+	player_->Initialize(model_, textureHandle_, &camera_);
+
+
 }
 
 void GameScene::Updatta() {
 
-	// 更新処理の追加
-	Vector2 position = sprite_->GetPosition();
-	position.x += 2.0f;
-	position.y += 1.0f;
-
-	sprite_->SetPosition(position);
-
-	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
-		Audio::GetInstance()->StopWave(voiceHandle_);
-	}
-
-	debugCamera_->Update();
-
-
-#ifdef DEBUG
-	ImGui::Begin("Debug");
-	ImGui::Text("Yamato %d.%d.%d", 2025, 1, 30);
-
-	// 自分で入力
-	ImGui::InputFloat3("InputFloat3", inputFloat);
-	// スライダー
-	ImGui::SliderFloat3("SliderFloat3", inputFloat, 0.0f, 1.0f);
-
-	ImGui::ShowDemoWindow();
-
-	ImGui::End();
-#endif // DEBUG
-
-
+	//更新処理の追加
+	player_->Updata();
 }
 
 void GameScene::Draw() {
 
-	// 3Dでのらいんびょうが
-	PrimitiveDrawer::GetInstance()->DrawLine3d(
-	    {0, 0, 0},               // 始点
-	    {0, 10, 0},              // 終点
-	    {1.0f, 0.0f, 0.0f, 1.0f} // 色
-	);
-
-	// 描画処理の追加
-
-	////写真用
-	// Sprite::PreDraw();
-	// sprite_->Draw();
-	// Sprite::PostDraw();
-
-	// 3Dモデル描画用
+	//描画処理の追加
 	Model::PreDraw();
-	model_->Draw(worldTransform_, debugCamera_->GetCamera(), textureHandle_);
+	player_->Draw();
+
+	//model_->Draw(worldTransform_, camera_, textureHandle_);
 	Model::PostDraw();
+
 }
 
- GameScene::~GameScene() {
+//デストラクタ（解放エクササイズ）
+GameScene::~GameScene() { 
+	//モデルの解放
+	delete model_;
+	delete player_;
 
 	//解放エクササイズ
 	delete sprite_;
--- GameScene.h
+++ GameScene.h
@@ -1,26 +1,21 @@
-#pragma once
+﻿#pragma once
 #include "KamataEngine.h"
+#include"Player.h"
 
 class GameScene {
 
 public:
-	
-	//コンストラクタとデストラクタ
 	GameScene();
 	~GameScene();
 
-	KamataEngine::Sprite* sprite_ = nullptr;
-	KamataEngine::Model* model_ = nullptr;
-
-	//ワールドトランスフォーム
+	// ワールドトランスフォーム
 	KamataEngine::WorldTransform worldTransform_;
-	//カメラ
+	// カメラ
 	KamataEngine::Camera camera_;
 
-	//デバック用カメラ
-	KamataEngine::DebugCamera* debugCamera_ = nullptr;
+	//3Dモデルで必要なモデルの呼び出し
+	KamataEngine::Model* model_ = nullptr;
 
-	
 
 	// 初期化関数
 	void Initialize();
@@ -31,19 +26,10 @@ public:
 	// 描画処理
 	void Draw();
 
+	Player* player_ = nullptr;
+
 private:
+	uint32_t textureHandle_;
 
-	float inputFloat[3] = {0, 0, 0};
-
-	//画像の読み込み
-	uint32_t textureHandle_ = 0;
-
-	//サウンドデータ読み込み
-	uint32_t soundDataHandle_ = 0;
-	//音楽再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//プライベートでシーンごとの旋回用の処理関数を追加
-
-
+	// プライベートでシーンごとの旋回用の処理関数を追加
 };
